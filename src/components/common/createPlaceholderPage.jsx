4efe233fee82e51cbe541c5f07b1a64bb7baec77
osamaktk <osamaktk648@gmail.com>
2026-06-30 18:09:58 +0500

    fix: remove unused frontend placeholders

--- src/components/common/createPlaceholderPage.jsx
+++
@@ -1,8 +0,0 @@
-import PagePlaceholder from './PagePlaceholder';
-import { pageMeta } from '../../data/pageMeta';
-
-export function createPlaceholderPage(pageKey) {
-  return function PlaceholderPage() {
-    return <PagePlaceholder {...pageMeta[pageKey]} />;
-  };
-}
